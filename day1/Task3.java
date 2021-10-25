68a0ec02475d475219d487bb53f3cad79114b62b
pelcin <93120209+pelcin@users.noreply.github.com>
2021-10-25 16:57:25 +0300

    Update Task3.java

--- day1/Task3.java
+++ day1/Task3.java
@@ -5,6 +5,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        String variable_1 = "A", variable_2 = "T", variable_3 = "U";
+        char variable_1 = 'A', variable_2 = 'T', variable_3 = 'U';
     }
 }
