005068c86c033594d2a01634bd716defbebb382f
pelcin <93120209+pelcin@users.noreply.github.com>
2021-10-25 12:26:05 +0300

    Update task1.java

--- day1/task1.java
+++ day1/task1.java
@@ -1,1 +1,9 @@
+//Выведите в консоль слово "Привет" два раза. Каждое слово должно начинаться с новой.
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        System.out.println("Привет");
+    }
+}
 
